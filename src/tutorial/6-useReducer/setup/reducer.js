689f4cb71e8a2d719d840744dfc8a3b9d1469b23
Ryan Bourdais <ryanbourdais@outlook.com>
2021-12-18 10:38:40 -0600

    completed useReducer module

---
+++ src/tutorial/6-useReducer/setup/reducer.js
@@ -0,0 +1,37 @@
+export const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "value added",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return {
+      ...state,
+      // people: state.people,
+      isModalOpen: true,
+      modalContent: "Please Enter Value",
+    };
+  }
+  if (action.type === "CLOSE_MODAL") {
+    return {
+      ...state,
+      isModalOpen: false,
+    };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    const newPeople = state.people.filter(
+      (person) => person.id !== action.payload
+    );
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "value removed",
+    };
+  }
+  throw new Error("no matching action type");
+};
